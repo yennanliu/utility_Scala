3e1ab3365ffb0c57265a0cba2f37e7d42db0e880
yennanliu <f339339@gmail.com>
2019-08-25 15:47:11 +0800

    add scala class demo

---
+++ ClassDemo.scala
@@ -0,0 +1,35 @@
+object ClassDemo{
+
+    def main(args:Array[String]){
+
+        var s = new Student1();  
+        println("Student1")
+        println("id : " + s.id + "\n" + "name : " + s.name);
+
+        var s2 = new Student2(101,"Raju"); 
+        println("Student2")
+        s2.show();  
+
+        var c = new Car(); 
+        println("Car1")
+        println("type : " + c.v_id + "\n" + "price : " + c.price)
+    }
+
+    class Student1{
+        var id:Int = 10; 
+        var name:String=null; 
+    }
+
+    class Student2(id:Int, name:String){
+
+        def show(){
+            println(id + " " + name)
+        }
+    }
+
+    class Car{
+        var v_id:Int = 10; 
+        var price:Int=999; 
+    }
+
+}
