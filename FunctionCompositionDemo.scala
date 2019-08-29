4a17c4bb661c397f3040bd13f4202da645469515
yennanliu <f339339@gmail.com>
2019-08-29 14:04:39 +0800

    add scala func without return value demo

--- FunctionCompositionDemo.scala
+++ FunctionCompositionDemo.scala
@@ -3,6 +3,7 @@ object FunctionCompositionDemo{
     def main(args: Array[String]) = {
         var result = multiply2(add2(100))
         println(result)
+        printMe()
     }
 
     def add2(a:Int):Int={
@@ -11,6 +12,10 @@ object FunctionCompositionDemo{
 
     def multiply2(a:Int):Int={
         a*2 
+    }
+
+    def printMe(): Unit={       // if no any return, use Unit, similar as void in Java 
+        println("Helo Scala!")
     }
 
 }
