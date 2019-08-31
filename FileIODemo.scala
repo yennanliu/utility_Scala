77cdcc3b9740af1b943116b1cd8c9596564e6fb9
yennanliu <f339339@gmail.com>
2019-08-31 10:31:53 +0800

    format code

--- FileIODemo.scala
+++ FileIODemo.scala
@@ -2,22 +2,22 @@ import scala.io.Source
 import java.io._
 
 object FileIODemo {
-   def main(args: Array[String]) {
-      println("read from file... (README.md)" )
-      print_file()
-      println("write to file... (scala_writed_file.txt)" )
-      write_file()
-   }
+  def main(args: Array[String]) {
+    println("read from file... (README.md)")
+    print_file()
+    println("write to file... (scala_writed_file.txt)")
+    write_file()
+  }
 
-   def print_file(){
-	  Source.fromFile("README.md" ).foreach{ 
-         print 
-      }
-   }
+  def print_file() {
+    Source.fromFile("README.md").foreach {
+      print
+    }
+  }
 
-   def write_file(){
-   	  val writer = new PrintWriter(new File("scala_writed_file.txt" ))
-   	  writer.write("this is from scala IO")
-      writer.close()
-   }
+  def write_file() {
+    val writer = new PrintWriter(new File("scala_writed_file.txt"))
+    writer.write("this is from scala IO")
+    writer.close()
+  }
 }
