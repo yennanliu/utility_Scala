c69f7abb25c0ec9ba112347728ac949d22fd07e2
yennanliu <f339339@gmail.com>
2019-11-19 09:26:40 +0800

    remove no need code test_UserDefinedFuncDemo2.scala

--- src/test/scala/test_UserDefinedFuncDemo2.scala
+++ src/test/scala/test_UserDefinedFuncDemo2.scala
@@ -21,25 +21,4 @@ class test_my_help_func extends FlatSpec {
     var result = my_help_func.my_product_func(x, y)
     assert ( result === 2 )
     }
-
 }
-
-
-// class test_UserDefinedFuncDemo2 extends FlatSpec {
-
-//   "A Stack" should "pop values in last-in-first-out order" in {
-//     val stack = new Stack[Int]
-//     stack.push(1)
-//     stack.push(2)
-//     assert(stack.pop() === 2)
-//     assert(stack.pop() === 1)
-//   }
-
-//   it should "throw NoSuchElementException if an empty stack is popped" in {
-//     val emptyStack = new Stack[String]
-//     assertThrows[NoSuchElementException] {
-//       emptyStack.pop()
-//     }
-//   }
-// }
-
