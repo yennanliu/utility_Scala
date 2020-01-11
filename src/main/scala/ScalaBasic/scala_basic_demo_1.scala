aec46458496aff5b0eeaec93b98462b7039f5771
yennanliu <f339339@gmail.com>
2020-01-11 13:09:37 +0800

    add scala basic pt 1 : /scala_basic_demo_1.scala

--- src/main/scala/ScalaBasic/scala_basic_demo_1.scala
+++ src/main/scala/ScalaBasic/scala_basic_demo_1.scala
@@ -9,9 +9,33 @@ object scala_basic_demo_1{
         if (1 > 3) println("Impossible!") else println("The world makes sense.") 
 
         if (1 > 3) { 
-            println("Make senses") 
-            }  else { println("it's Impossible") }
+            println("it's Impossible") 
+            }  else { println("make sense") }
         
+        // 2) Matching - like switch in other languages
+        val y = 3 
+        y match {
+
+            case 1 => println("one")
+            case 2 => println("two")
+            case 3 => println("three")
+            case _ => println("sth else") // other case
+        }
+
+        // 3) For loop 
+        var x = 10 
+        while (x > 0){
+            println(x)
+            x -= 1 
+        }
+
+        var z = 0 
+        do { println(z); z += 1} while (z <= 10)
+
+
+        //4)  "Returns" the final value in a block automatically
+        {var u = 10; u + 20}
+        println({val u = 10; u + 20})  
 
     }
 }
