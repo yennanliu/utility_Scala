b798131b689fbb261fe07d9bb8f26704ae4f3aa1
yennanliu <f339339@gmail.com>
2020-01-13 14:21:58 +0800

    add SparkBasic

---
+++ src/main/scala/SparkBasic/spark_basic_demo_1.scala
@@ -0,0 +1,26 @@
+package SparkBasic
+
+object spark_basic_demo_1{
+
+    def main(args: Array[String]){
+
+        // 0) define a rdd 
+        val rdd = sc.parallelize(List(1,2,3,4))
+
+        // 1) collect the rdd 
+        rdd.collect()
+
+        // 2) map a lambda func to RDD 
+        val squares = rdd.map( x => x * x)
+
+        // 3) map a func to RDD
+        def squareIt( x : Int): Int = { 
+            return x * x}
+
+        rdd.map(squareIt).collect()
+
+        // 4) reduce as a action to the RDD
+        //rdd.map(squareIt).map( x => (x,1)).reduceByKey {case (x, y) => x + y }
+
+    }
+}
