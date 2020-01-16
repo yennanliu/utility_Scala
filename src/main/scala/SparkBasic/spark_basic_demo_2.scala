53d3a88d52b0315a78616e393f161e7fd3ce95d2
yennanliu <f339339@gmail.com>
2020-01-16 16:29:10 +0800

    add code comment (spark_basic_demo_2)

--- src/main/scala/SparkBasic/spark_basic_demo_2.scala
+++ src/main/scala/SparkBasic/spark_basic_demo_2.scala
@@ -21,9 +21,18 @@ object spark_basic_demo_2{
         }
 
         val lines = sc.textFile("data/fakefriends.csv")
+        // lines --> Array(0,Will,33,385, 1,Jean-Luc,26,2, 2,Hugh,55,221...) --> (user_id, user_name, age, numFriends)
+        
         val rdd = lines.map(parseLine)
-        val totalsByAge = rdd.mapValues( x => (x,1) ).reduceByKey( (x,y) => (x._1 + y._1, x._2 + y._2)) // (x,y) => (number of friends, number of count by age by friends)
+        //  rdd --> Array((33,385), (26,2), (55,221), (40,465))  : (age, numFriends) 
+        
+        val totalsByAge = rdd.mapValues( x => (x,1) ).reduceByKey( (x,y) => (x._1 + y._1, x._2 + y._2))  // (x._1 + y._1, x._2 + y._2)  --> (number of friends, number of count by age by friends)
+        // rdd.mapValues( x => (x,1) )  --> Array((33,(385,1)), (26,(2,1)), (55,(221,1))...))  : (age, (numFriends,1))
+        // rdd.mapValues( x => (x,1) ).reduceByKey( (x,y) => (x._1 + y._1, x._2 + y._2))  -->  Array((33,(385,1)), (26,(2,1)), (55,(221,1)), (40,(465,1))...)))  : (age, ( sum of numFriends , count of this combination))
+
         val averageByAge = totalsByAge.mapValues(x => x._1 / x._2)
+        // Array((34,245), (52,340), (56,306) .. )) :  (age, avg_friends_by_age)
+
         val result = averageByAge.collect()
 
         result.sorted.foreach(println)
