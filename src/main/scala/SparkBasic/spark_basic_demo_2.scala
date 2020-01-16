5fdb1c25c38d8096d445559eca992f9bf2a6a357
yennanliu <f339339@gmail.com>
2020-01-16 15:42:43 +0800

    add more data transform step : spark_basic_demo_2

--- src/main/scala/SparkBasic/spark_basic_demo_2.scala
+++ src/main/scala/SparkBasic/spark_basic_demo_2.scala
@@ -22,7 +22,11 @@ object spark_basic_demo_2{
 
         val lines = sc.textFile("data/fake_friend.csv")
         val rdd = lines.map(parseLine)
+        val totalsByAge = rdd.mapValues( x => (x,1) ).reduceByKey( (x,y) => (x._1 + y._1, x._2 + y._2)). // (x,y) => (number of friends, number of count by age by friends)
+        val averageByAge = totalsByAge.reduceByKey( x => x._1/x._2 )
+        val result = averageByAge.collect()
 
+        result.sorted.foreach(println)
 
     }
 }
