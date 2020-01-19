ab31c1c176d1ebed9e71b9917737d5a904343c3b
yennanliu <f339339@gmail.com>
2020-01-19 20:31:24 +0800

    update sort code (spark_basic_demo_4_extra.scala)

--- src/main/scala/SparkBasic/spark_basic_demo_4_extra.scala
+++ src/main/scala/SparkBasic/spark_basic_demo_4_extra.scala
@@ -21,11 +21,19 @@ object spark_basic_demo_4_extra{
 
         val lowercaseWords = words.map( x => x.toLowerCase() )
 
-        val wordcounts = lowercaseWords.countByValue()
-        // val wordcounts = words.map( x => (x,1)).reduceByKey( (x, y) => x+y)
+        //val wordcounts = lowercaseWords.countByValue()
+        val wordcounts = words.map( x => (x,1)).reduceByKey( (x, y) => x+y)
 
-        for (wordcount <- wordcounts){ println(wordcount) } 
-        // wordcounts.foreach(println)
+        val wordcountSorted = wordcounts.map( x => (x._2, x._1)).sortByKey() // here we map wordcounts as (value, key), since we want to sort the result by value via the "sortByKey()" method
+
+        //for (wordcount <- wordcounts){ println(wordcount) } 
+        //for (wordcount <- wordcountsSorted.sorted()){ println(wordcount) }
+        //wordcountSorted.collect().sorted.foreach(println)
+        for (result <- wordcountSorted) {
+              val count = result._1
+              val word = result._2
+              println(s"$word: $count")
+        }
 
 
     }
