6a191b798a3375f41af1c2727a274a1042097d8f
yennanliu <f339339@gmail.com>
2020-01-19 21:59:38 +0800

    backup code (TotalSpentByCustomer.scala)

--- src/main/scala/SparkBasic/TotalSpentByCustomer.scala
+++ src/main/scala/SparkBasic/TotalSpentByCustomer.scala
@@ -29,13 +29,17 @@ object TotalSpentByCustomer{
 
         val rdd = lines.map(parseLine)
 
-        //val customerSpendAmount = rdd.map( x => (x._1, (x._2, x._3))).reduceByKey( (x,y) => x*y)
-        //customerSpendAmount.take(10)
-
+        // customer Spend Amount (in count)
         val customerSpendAmount = rdd.map( x => (x._1, x._2)).reduceByKey( (x,y) => x + y )
-
         val result = customerSpendAmount.collect()
-        result.foreach(println)
+        result.sorted.foreach(println)
+
+        val sortedcustomerSpendAmount = customerSpendAmount.map( x => (x._2, x._1)).sortByKey()
+        sortedcustomerSpendAmount.foreach(println)
+
+        // customer Spend Amount (in dollar)
+        // val customerTotalSpendAmount = rdd.map( x => (x._1, (x._2, x._3))).reduceByKey( (x,y) => (x._1 + y._1, x._2 + y._2)) //.reduceByKey( (x,y) => y._1 + y._2)
+        // customerTotalSpendAmount.take(10)
 
     }
 }
