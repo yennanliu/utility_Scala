43d8c5d23db83306079cf6c4bc3365597b4d51ef
yennanliu <f339339@gmail.com>
2020-02-07 19:46:03 +0800

    add WarDataDigest spark code

--- src/main/scala/WarDataDigest/WarDataOverview.scala
+++ src/main/scala/WarDataDigest/WarDataOverview.scala
@@ -2,6 +2,7 @@ package WarDataDigest
 
 import org.apache.spark._
 import org.apache.spark.SparkContext._
+import org.apache.spark.sql.SparkSession
 import org.apache.log4j._
 
 
@@ -14,7 +15,17 @@ object WarDataOverview{
 
         val sc = new SparkContext("local[*]", "WarDataOverview")
 
-        val gameLog = sc.textFile("data/data/war_data_sample.json")
+        val spark = SparkSession.builder.appName("WarDataOverview").config("spark.master", "local").getOrCreate()
+        
+        import spark.implicits._
+
+        val gameLogDF = spark.read.json("data/war_data_sample.json")
+
+        gameLogDF.printSchema()
+
+        gameLogDF.take(10)
+
+        val gameLog = gameLogDF.rdd
 
         gameLog.take(10)
 
