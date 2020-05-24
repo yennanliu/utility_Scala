48559c95517b1c844991dd9d95e5643006da9aeb
yennanliu <f339339@gmail.com>
2020-05-24 14:49:28 +0800

    add src/main/scala/ScalaBasic/ExtendsDemo.scala

---
+++ src/main/scala/ScalaBasic/ExtendsDemo.scala
@@ -0,0 +1,9 @@
+package ScalaBasic
+
+// https://stackoverflow.com/questions/11667630/difference-between-using-app-trait-and-main-method-in-scala/51909980
+// https://stackoverflow.com/questions/40555931/why-should-a-scala-program-have-a-main-method-or-extend-the-app-trait
+object ExtendsDemo extends App {
+
+	Console.println("hello world : ")
+
+}
