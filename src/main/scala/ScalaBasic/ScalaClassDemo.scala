a4d3c3b162344ecad739596afc26d3ecb06bd152
yennanliu <f339339@gmail.com>
2020-07-02 09:28:08 +0800

    add scala setter, getter demo (ScalaBasic/ScalaClassDemo.scala

--- src/main/scala/ScalaBasic/ScalaClassDemo.scala
+++ src/main/scala/ScalaBasic/ScalaClassDemo.scala
@@ -9,10 +9,29 @@ object ScalaClassDemo{
         def current() = value
     }
 
+    class Person{
+        private var privateAge = 0 // set as private var
+
+        // "getter" method in scala
+        def age = privateAge 
+
+        // "setter" method in scala
+        def age_= (newValue: Int): Unit = {
+            if (newValue > privateAge) privateAge = newValue
+          }
+    }
+
     def main(args: Array[String]){ 
+        // use class Counter
         val myCounter = new Counter // or new Counter()
         myCounter.increment()
         println(myCounter.current())
+
+        // use Person and its getter, setter method
+        val fred = new Person
+        fred.age = 30 
+        fred.age = 21
+        println(fred.age) //30
 
     }
 }
