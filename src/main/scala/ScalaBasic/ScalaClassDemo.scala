2fc9bcc285deb9a2edfdb3bd13df810e289693fe
yennanliu <f339339@gmail.com>
2020-07-02 09:42:49 +0800

    update ScalaBasic/ScalaClassDemo.scala

--- src/main/scala/ScalaBasic/ScalaClassDemo.scala
+++ src/main/scala/ScalaBasic/ScalaClassDemo.scala
@@ -2,13 +2,14 @@ package ScalaBasic
 
 
 object ScalaClassDemo{
-
+    // simple class 
     class Counter{
         private var value = 0 
         def increment(): Unit = {value += 1}
         def current() = value
     }
 
+    // class with "getter", "setter" method
     class Person{
         private var privateAge = 0 // set as private var
 
@@ -20,6 +21,22 @@ object ScalaClassDemo{
             if (newValue > privateAge) privateAge = newValue
           }
     }
+
+    // class with "getter" method only
+    class Message{
+        val timestamp = new java.util.Date
+    }
+
+    // class access another private val in the same class
+    // TODO :  check again 
+    class Counter2{
+        private var value = 0
+        def increment(): Unit = { value += 1 }
+        def isLess(other: Counter2) = value < other.value
+    }
+
+    // Bean property
+    //import scala.reflect.BeanProperty
 
     def main(args: Array[String]){ 
         // use class Counter
