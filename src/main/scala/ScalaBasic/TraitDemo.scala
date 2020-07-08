662fcec3b359e002cf761516f1472fd3d4c71ec2
yennanliu <f339339@gmail.com>
2020-07-08 10:00:54 +0800

    update scala/ScalaBasic/TraitDemo.scala

--- src/main/scala/ScalaBasic/TraitDemo.scala
+++ src/main/scala/ScalaBasic/TraitDemo.scala
@@ -13,4 +13,23 @@ object TraitDemo extends App {
     val x = new ConsoleLogger
     println(x.log("123"))
 
+    // ConsoleLogger can be a trait as well
+    trait ConsoleLogger2 {
+        def log2(msg: String): Unit = {println(msg)}
+    } 
+
+    // // extend above trait (ConsoleLogger2)
+    // class SavingsAccount extends Account with ConsoleLogger2{
+    //     def withdraw(amount: Double, balance: Double): Unit = {
+    //         if (amount > balance) log("insufficienct fund")
+    //         else balance = balance - amount
+    //     }
+
+    // }
+
+    // can also add different traita when declare a value
+    val y = new ConsoleLogger with ConsoleLogger2
+    println(y.log("helloworld"))
+    println(y.log2("helloworld"))
+
 }
