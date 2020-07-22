d64a0d9e809790f44f486246cebb44c47c7e8e41
yennanliu <f339339@gmail.com>
2020-07-22 10:12:26 +0800

    add more examples (ScalaBasic/AbstractClass.scala

--- src/main/scala/ScalaBasic/AbstractClass.scala
+++ src/main/scala/ScalaBasic/AbstractClass.scala
@@ -1,9 +1,16 @@
+// https://www.youtube.com/watch?v=ONmBOPgTKYw
+
 package ScalaBasic
 
 object AbstractClass{
 
-    def main(args: Array[String]){  
+    def main(args: Array[String]){
+        /*
+         * example 1
+         */
+        // Item is a abstract class
         abstract class Item
+        // extends abstract class
         case class Article(description: String, price: Double) extends Item
         case class Bundle(description: String, discount: Double, item: Item*) extends Item
 
@@ -13,5 +20,19 @@ object AbstractClass{
         println(bundle_a)
         println(article_a)
         
+        /*
+         * example 2
+         */
+        abstract class Polygon{
+            def area: Double
+        }
+        // extends abstract class
+        class Rectangle(var width: Double, var height: Double) extends Polygon{
+            def area = width * height
+        }
+
+        val myarea = new Rectangle(3,4)
+        println(myarea.area)
+
     }
 }
