d9ef2583909630deeed59e15c158b7fcafa0508d
yennanliu <f339339@gmail.com>
2020-07-29 09:18:05 +0800

    add more final example (OverrideDemo.scala), backup sbt meta

--- src/main/scala/ScalaBasic/OverrideDemo.scala
+++ src/main/scala/ScalaBasic/OverrideDemo.scala
@@ -1,7 +1,10 @@
+// https://www.geeksforgeeks.org/scala-final/
+
 package ScalaBasic
 
 object OverrideDemo extends App {
 
+    // example 1
     class Person(val name: String){
         override def toString = getClass.getName + "[name=" + name + "]"
     }
@@ -17,5 +20,76 @@ object OverrideDemo extends App {
     val secretUser = new SecretAgent("tom")
     println(secretUser.name)
     println(secretUser.toString)
+    
+    // example 2
+    final class Car(val name: String){
+    println("this is final class, so can't be overridden")
 
+    }
+
+    // can't override class Car
+    // class Mycar(val codeanme: String) extends Car(codeanme){
+    //     println(codeanme)
+    // }
+
+    // example 3 : final variable 
+    // Scala program of using final variable 
+    class Shapes 
+    { 
+        // define final variable 
+        final val area:Int = 60
+    } 
+    // class Rectangle extends Shapes 
+    // { 
+    //     override val area:Int = 100
+    //     def display() 
+    //     { 
+    //         println(area) 
+    //     } 
+    // } 
+
+    // Creating object 
+    // object GFG 
+    // { 
+    //     // Main method 
+    //     def run(): Unit =
+    //     { 
+    //         var b = new Rectangle() 
+    //         b.display() 
+    //     } 
+    // } 
+
+    // example 4 : final class
+    // Scala program of using final class 
+    final class Shapes2
+    { 
+        // Final variables and functions 
+        val height:Int = 0
+        val width :Int =0
+        final def CalArea() 
+        { 
+        } 
+    } 
+    // class Rectangle2 extends Shapes2 
+    // { 
+    //     // Cannot inherit Shapes class 
+    //     override def CalArea() 
+    //     { 
+    //         val area:Int = height * width 
+    //         println(area) 
+    //     } 
+    // } 
+
+    // // Creating Object 
+    // object GFG2 
+    // { 
+    //     // Main method 
+    //     def run() 
+    //     { 
+    //         var b = new Rectangle() 
+    //         b.CalArea() 
+            
+    //     } 
+    // } 
+     
 }
