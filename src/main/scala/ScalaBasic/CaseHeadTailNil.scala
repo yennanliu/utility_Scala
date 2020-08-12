67240f93da00f872b82baf29b7053d4f1d387563
yennanliu <f339339@gmail.com>
2020-08-12 23:39:06 +0800

    add min implement example (CaseHeadTailNil.scala)

--- src/main/scala/ScalaBasic/CaseHeadTailNil.scala
+++ src/main/scala/ScalaBasic/CaseHeadTailNil.scala
@@ -12,17 +12,37 @@ object CaseHeadTailNil extends App{
         case _ :: tail => flatten(tail)
     }
 
-    def len (l:List[Int]):Int = 
+    def len(l: List[Int]): Int = 
       l match {
         case Nil    => 0           // return 0 if the list is null 
         case _ :: t => 1 + len(t)  // len += 1 if the element of list is not null
       }
 
+    def min(l: List[Int]): Int = 
+      l match {
+          case Nil => throw new IllegalArgumentException  // return IllegalArgumentException if list is null
+          case n :: Nil  => n    // return n if only first element is not null, but rest of them are null
+          case n :: t => {       // if both first (head) and the rest of elements (tail) are not null, then compare with each them and find the min
+            val m = min(t)
+            if (n < m) n else m
+          }
+      }
+
+    // *** important properties  *** //
     val x = List(1,2,3)
     println(x.head)     // 1
     println(x.tail)     // List(2, 3)
     println(x.length)   // 3
     println(Nil == List())  // true 
+
+    val y = 6 :: Nil 
+    println(y)     // List(6)
+    val z = 5 :: y
+    println(z)    // List(5,6)
+    println(z.tail eq y)   // true
+
+    val s = 3 :: 4 :: Nil
+    println(s)   // List(3,4)
 
     // test 1 
     println(flatten(List(List("one",9,8),3,"str",4,List(true,77,3.2)))) // List(9, 8, 3, 4, 77)
@@ -32,5 +52,11 @@ object CaseHeadTailNil extends App{
     // test 2
     println(len(List()))
     println(len(List(1,2,3)))
+
+    // test 3
+    //println(min(List()))    // IllegalArgumentException
+    println(min(List(1,2,3)))
+    println(min(List(10,9,8)))
+
 
 }
