7820852e5011dedef3986d0a469dc223115148f9
yennanliu <f339339@gmail.com>
2020-08-14 00:02:11 +0800

    add more example (GenericTypeParam.scal), update sbt meta

--- src/main/scala/ScalaBasic/GenericTypeParam.scala
+++ src/main/scala/ScalaBasic/GenericTypeParam.scala
@@ -2,6 +2,7 @@ package ScalaBasic
 
 // https://docs.scala-lang.org/tour/generic-classes.html
 // https://stackoverflow.com/questions/36977738/what-does-the-syntax-functiont-means-in-scala
+// https://www.geeksforgeeks.org/generic-classes-in-scala/#:~:text=In%20Scala%2C%20forming%20a%20Generic,be%20Generic%20Classes%20in%20Scala.
 
 object GenericTypeParam extends App {
 
@@ -30,5 +31,38 @@ object GenericTypeParam extends App {
     println(stack2.pop)  // prints "b"
     println(stack2.pop)  // prints "a"
 
-    
+
+    // example 2
+    // T as type parameter identifier
+    class ProcessWord[T] {
+        def print(x : T) = {
+            println("***" +  x.toString() + "***")
+        }
+    }
+
+    val processw = new ProcessWord[Int]
+    println(processw.print(123))
+
+    val processw2 = new ProcessWord[String]
+    println(processw2.print("abc"))
+
+    // example 3 
+    abstract class Divide[z]{
+        def divide(u: z, v: z): z
+    }
+
+    class intDivide extends Divide[Int]{
+        def divide(u: Int, v: Int):  Int = u / v
+    }
+
+    class doubleDivide extends Divide[Double]{
+        def divide(u: Double, v: Double):  Double = u / v
+    }
+
+    val q = new intDivide().divide(25, 5) 
+    val r = new doubleDivide().divide(21.0, 5.0) 
+
+    // Displays output  
+    println(q) 
+    println(r) 
 }
