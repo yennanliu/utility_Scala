83856e7c9d01018d10d5d49859bbf9180bab9f43
yennj12 <f339339@gmail.com>
2020-08-14 09:22:14 +0800

    Add files via upload

---
+++ src/main/scala/ScalaDev/YenLib.scala
@@ -0,0 +1,16 @@
+package ScalaBasic
+
+class YenLib {
+
+  def printsth(): Unit = {
+    println("this is printsh func")
+  }
+
+  def largerSmaller(x: Float): Boolean = {
+    x > 100
+  }
+
+  case class ShopUrl(shop_id: Int, shop_url: String)
+
+
+}
---
+++ src/main/scala/ScalaDev/intellj_script.scala
@@ -0,0 +1,45 @@
+package ScalaBasic
+
+import java.io.{BufferedWriter, File, FileWriter}
+
+import org.slf4j.LoggerFactory
+
+import scala.io.Source
+import scala.util.Try
+import ScalaBasic.YenLib
+import ScalaBasic.common
+
+object intellj_script extends App{
+  val Logger = LoggerFactory.getLogger(this.getClass)
+  println("this is intllj script")
+  for (i <- 1 to 20)
+    println(i)
+
+  println("run from YenLib")
+  val yenlib = new YenLib
+  yenlib.printsth()
+
+  val y = yenlib.largerSmaller(16)
+  println(y)
+
+  println(yenlib.ShopUrl)
+
+  private def fileToMap(filename: String): Map[Int, String] = {
+    (for {
+      line <- Source.fromFile(filename).getLines
+      s = line.split("\t")
+    } yield (s(0).toInt, s(1))).toMap[Int, String]
+  }
+
+  val filename = "/home/yennan.liu/yen_scala"
+
+//  val z = fileToMap(filename)
+//  println (z)
+
+  Logger.info( "---- \n" + "this is runnign log \n" + "----")
+
+  val com = new common
+  val mytime = com.YYYYMMDDHH("2020", "11", "11", "11")
+  println(mytime)
+
+}
