d1df70221f4d575501e880d3f62e4688a547cc6a
yennanliu <f339339@gmail.com>
2020-08-21 00:06:55 +0800

    fix syntax errror (CaseClass.scala), delete /target

--- src/main/scala/ScalaBasic/CaseClass.scala
+++ src/main/scala/ScalaBasic/CaseClass.scala
@@ -21,13 +21,13 @@ object CaseClass extends App{
     message1.sender = "travis@washington.us"  // this line does not compile
 
     // example 3 : Comparison
-    case class Message(sender: String, recipient: String, body: String)
+    //case class Message(sender: String, recipient: String, body: String)
     val message2 = Message("jorge@catalonia.es", "guillaume@quebec.ca", "Com va?")
     val message3 = Message("jorge@catalonia.es", "guillaume@quebec.ca", "Com va?")
     val messagesAreTheSame = message2 == message3  // true
 
     // example 4 : Copy
-    case class Message(sender: String, recipient: String, body: String)
+    //case class Message(sender: String, recipient: String, body: String)
     val message4 = Message("julien@bretagne.fr", "travis@washington.us", "Me zo o komz gant ma amezeg")
     val message5 = message4.copy(sender = message4.recipient, recipient = "claire@bourgogne.fr")
     message5.sender  // travis@washington.us
