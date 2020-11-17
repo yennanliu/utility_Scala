4a544aa2575e3e8cca7a328d2fa13f94769b97c4
yennanliu <f339339@gmail.com>
2020-11-17 08:19:01 +0800

    move WarDataDigest, SparkDemo, .. to sparkBasic/src/scala, comment src/main/scala/ScalaDev

--- src/main/scala/ScalaDev/common.scala
+++ src/main/scala/ScalaDev/common.scala
@@ -1,16 +1,16 @@
-package ScalaDev
+// package ScalaDev
 
-import org.joda.time.format.{DateTimeFormat, DateTimeFormatter, ISODateTimeFormat}
-import org.joda.time.{DateTime, DateTimeZone, Hours, Years}
-import org.slf4j.LoggerFactory
+// import org.joda.time.format.{DateTimeFormat, DateTimeFormatter, ISODateTimeFormat}
+// import org.joda.time.{DateTime, DateTimeZone, Hours, Years}
+// import org.slf4j.LoggerFactory
 
-class common {
-  val dateHourFormat = DateTimeFormat.forPattern("yyyy-MM")
-  private val logger = LoggerFactory.getLogger(this.getClass.getName)
+// class common {
+//   val dateHourFormat = DateTimeFormat.forPattern("yyyy-MM")
+//   private val logger = LoggerFactory.getLogger(this.getClass.getName)
 
-  case class YYYYMMDDHH(yyyy: String, mm: String, dd: String, hh: String)  {
-    val yyyymmdd: Int = s"$yyyy$mm$dd".toInt
-    yyyymmdd
-  }
+//   case class YYYYMMDDHH(yyyy: String, mm: String, dd: String, hh: String)  {
+//     val yyyymmdd: Int = s"$yyyy$mm$dd".toInt
+//     yyyymmdd
+//   }
 
-}
+// }
--- src/main/scala/ScalaDev/intellj_script.scala
+++ src/main/scala/ScalaDev/intellj_script.scala
@@ -1,45 +1,45 @@
-package ScalaDev
+// package ScalaDev
 
-import java.io.{BufferedWriter, File, FileWriter}
+// import java.io.{BufferedWriter, File, FileWriter}
 
-import org.slf4j.LoggerFactory
+// import org.slf4j.LoggerFactory
 
-import scala.io.Source
-import scala.util.Try
-import ScalaDev.YenLib
-import ScalaDev.common
+// import scala.io.Source
+// import scala.util.Try
+// import ScalaDev.YenLib
+// import ScalaDev.common
 
-object intellj_script extends App{
-  val Logger = LoggerFactory.getLogger(this.getClass)
-  println("this is intllj script")
-  for (i <- 1 to 20)
-    println(i)
+// object intellj_script extends App{
+//   val Logger = LoggerFactory.getLogger(this.getClass)
+//   println("this is intllj script")
+//   for (i <- 1 to 20)
+//     println(i)
 
-  println("run from YenLib")
-  val yenlib = new YenLib
-  yenlib.printsth()
+//   println("run from YenLib")
+//   val yenlib = new YenLib
+//   yenlib.printsth()
 
-  val y = yenlib.largerSmaller(16)
-  println(y)
+//   val y = yenlib.largerSmaller(16)
+//   println(y)
 
-  println(yenlib.ShopUrl)
+//   println(yenlib.ShopUrl)
 
-  private def fileToMap(filename: String): Map[Int, String] = {
-    (for {
-      line <- Source.fromFile(filename).getLines
-      s = line.split("\t")
-    } yield (s(0).toInt, s(1))).toMap[Int, String]
-  }
+//   private def fileToMap(filename: String): Map[Int, String] = {
+//     (for {
+//       line <- Source.fromFile(filename).getLines
+//       s = line.split("\t")
+//     } yield (s(0).toInt, s(1))).toMap[Int, String]
+//   }
 
-  val filename = "/home/yennan.liu/yen_scala"
+//   val filename = "/home/yennan.liu/yen_scala"
 
-//  val z = fileToMap(filename)
-//  println (z)
+// //  val z = fileToMap(filename)
+// //  println (z)
 
-  Logger.info( "---- \n" + "this is runnign log \n" + "----")
+//   Logger.info( "---- \n" + "this is runnign log \n" + "----")
 
-  val com = new common
-  val mytime = com.YYYYMMDDHH("2020", "11", "11", "11")
-  println(mytime)
+//   val com = new common
+//   val mytime = com.YYYYMMDDHH("2020", "11", "11", "11")
+//   println(mytime)
 
-}
+// }
