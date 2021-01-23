b8eb75b3b8cce95112719f4d9c22ecd0262f1cdf
yennanliu <f339339@gmail.com>
2021-01-23 10:58:14 +0800

    update ScalaBasic/pkgObject.scala

--- src/main/scala/ScalaBasic/pkgObject.scala
+++ src/main/scala/ScalaBasic/pkgObject.scala
@@ -8,17 +8,20 @@ package ScalaBasic {
    *  2) package$.class
    *  when we run "sayOK" method, it actually call the static method package$.class
    */
-  package object oa {
+  package object oa {  // package object oa and package oa need to be under the same "package ScalaBasic "
     def sayOK: Unit = {
       println("say OKKKKKKKK")
     }
+
+    val value = 100.01
   }
 
   package oa {
     object A{
       def main(args: Array[String]): Unit = {
         // run
-        sayOK
+        sayOK // equal as : MODULE$sayOK in the package$.class under the hood
+        println(value) // equal as : MODULE$sayOK.value() in the package$.class under the hood
       }
     }
   }
