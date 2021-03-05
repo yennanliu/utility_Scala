c44b46a67ab923994d6a6f25ba14c71990b764b5
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-03-05 19:49:41 +0800

    add /TraitDemo3.scala

---
+++ src/main/scala/ScalaBasic/TraitDemo3.scala
@@ -0,0 +1,39 @@
+package ScalaBasic
+
+// https://www.youtube.com/watch?v=k18Ea9ko07s&list=PLmOn9nNkQxJEqCNXBu5ozT_26xwvUbHyE&index=96
+
+object TraitDemo3 extends App {
+  // run
+  val c = new C
+  c.getConnect("root", "123")
+
+  val f = new F
+  f.getConnect("admin", "000")
+
+}
+
+// trait
+// make a specification that connect to DB
+trait trait03_1{
+  // abstract method
+  def getConnect(user:String, pwd:String):Unit
+}
+
+
+// class
+class A_a {}
+class B_b extends A_a {}
+class C extends A_a with trait03_1 {
+  // override here is optional
+  override def getConnect(user: String, pwd: String): Unit = {
+    println("connect to mysql ...")
+  }
+}
+
+class D {}
+class E extends D {}
+class F extends D with trait03_1 {
+  override def getConnect(user: String, pwd: String): Unit = {
+    println("connect to Oracle ...")
+  }
+}
