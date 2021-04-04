add37a838e19572fa0b45db37af7916efafd1b11
yennanliu <f339339@gmail.com>
2021-04-04 20:38:02 +0800

    update TupleDemo1.scala

--- src/main/scala/ScalaBasic/TupleDemo1.scala
+++ src/main/scala/ScalaBasic/TupleDemo1.scala
@@ -7,6 +7,8 @@ object TupleDemo1 extends App {
    *   1) make a tuple, (dtype = tuple4)
    *   2) tuple can only have up-to 22 elements (22 is maximum)
    *   3) get element in tuple :  tuple_1._1, tuple_1._2 .... (get 1st, 2nd element...)
+   *   4) can get element via productElement (index) as well
+   *   5) if want to looping over it, => need "iterator" (since tuple is not list)
    */
   val tuple_1 = (1,2,3, "hello")
   println(tuple_1)
@@ -14,6 +16,24 @@ object TupleDemo1 extends App {
   println(tuple_1._2)
   println(tuple_1._3)
   println(tuple_1._4)
+
+  println("================")
+
+  // or via productElement (start from 0)
+  println(tuple_1.productElement(0))
+  println(tuple_1.productElement(1))
+  println(tuple_1.productElement(2))
+  println(tuple_1.productElement(3))
+
+  println("================")
+
+  // for loop. since tuple is an one "set" -> we need iterator for looping it
+  println("looping : ")
+  for (item <- tuple_1.productIterator){
+    println(item)
+  }
+
+  println("================")
 
   val tuple_22 = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22)
   println(tuple_22)
