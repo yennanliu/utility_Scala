4336eb9bc5559684158b3777a8e45138f0fba1ab
yennanliu <f339339@gmail.com>
2021-06-06 09:41:09 +0800

    add CustomerCRM/ project

---
+++ src/main/scala/ScalaBasic/CustomerCRM/bean/Customer.scala
@@ -0,0 +1,13 @@
+package ScalaBasic.CustomerCRM.bean
+
+// https://www.youtube.com/watch?v=WUPrKqXyI30&list=PLmOn9nNkQxJEqCNXBu5ozT_26xwvUbHyE&index=177
+
+class Customer {
+  // attr
+  var id: Int = _
+  var name: String = _
+  var gender: String = _
+  var age: Short = _
+  var tel: String = _
+  var email : String = _
+}
---
+++ src/main/scala/ScalaBasic/CustomerCRM/test.scala
@@ -0,0 +1,7 @@
+package ScalaBasic.CustomerCRM
+
+// https://www.youtube.com/watch?v=WUPrKqXyI30&list=PLmOn9nNkQxJEqCNXBu5ozT_26xwvUbHyE&index=177
+
+object test {
+
+}
