5f54912a6e0dbc80002da6fdd0d3767ebea3c28b
yennanliu <f339339@gmail.com>
2021-07-01 08:17:33 +0800

    update/fix akka client

--- src/main/scala/AkkaDemo3YellowChicken/cilent/CustomerActor.scala
+++ src/main/scala/AkkaDemo3YellowChicken/cilent/CustomerActor.scala
@@ -1,6 +1,6 @@
 package AkkaDemo3YellowChicken.cilent
 
-import akka.actor.{Actor, ActorSelection, ActorSystem}
+import akka.actor.{Actor, ActorSelection, ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
 
 // https://www.bilibili.com/video/BV12N411R726?p=243&spm_id_from=pageDriver
@@ -8,7 +8,7 @@ import com.typesafe.config.ConfigFactory
 // entry point (main program)
 object CustomerActor extends App {
 
-  val (clientHost, clientPort, serverHost, serverPort) = ("127.0.0.1", 9999, "127.0.0.1", 9999)
+  val (clientHost, clientPort, serverHost, serverPort) = ("127.0.0.1", 9990, "127.0.0.1", 9999)
 
   // create config, includes protocol, ip, and port
   val config = ConfigFactory.parseString(
@@ -23,7 +23,10 @@ object CustomerActor extends App {
   private val clientActorSystem = ActorSystem("client", config)
 
   // create CustomerActor's instance and reference
+  val customerActorRef = clientActorSystem.actorOf(Props(new CustomerActor(serverHost, serverPort)), "customerActor")
 
+  // run customerActorRef ( run the Actor)
+  customerActorRef ! "start"
 }
 
 class CustomerActor(serverHost: String, serverPort: Int) extends Actor {
@@ -38,12 +41,18 @@ class CustomerActor(serverHost: String, serverPort: Int) extends Actor {
    */
 
   override def preStart(): Unit = {
+    println("preStart start ....")
+
     // get below server url from YellowChickenServer running (console)
     //serverActorRef = context.actorSelection("akka.tcp://server@127.0.0.1:9999")
     serverActorRef = context.actorSelection(s"akka.tcp://server@${serverHost}:${serverPort}/user/YellowChickenServer")
+
+    println("serverActorRef = " + serverActorRef)
   }
 
-  override def receive: Receive = ???
+  override def receive: Receive = {
+    case "start" => println("client start !!! ready to serve")
+  }
 }
 
 
