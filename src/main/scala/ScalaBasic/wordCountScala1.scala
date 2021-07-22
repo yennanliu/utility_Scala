a897d6effd92af312152126cddd2c6fe6c060ef5
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-07-22 10:36:19 +0800

    update wordCountScala1.scala , readme

--- src/main/scala/ScalaBasic/wordCountScala1.scala
+++ src/main/scala/ScalaBasic/wordCountScala1.scala
@@ -5,7 +5,7 @@ package ScalaBasic
 /**
  *   Word Count 1 : map-reduce
  *
- *   given a lines
+ *   given a lines :
  *   val lines = List("hello helo helo helllo", "world, worl, wor, world", "helo, helo")
  *
  *   plz find the counts of each word in the lines
@@ -25,16 +25,29 @@ object wordCountScala1 extends App {
   val _line2 = _line1.map(x => (x,1))
   println(_line2)
 
-  // step 3) grouping with same "key" (groupBy)
-  //val _line3 = _line2.map(a => (a._2, a._1))
-  val _line3 = _line2.groupBy((x) => x._1)
+  /** step 3) grouping with same "key" (groupBy) */
+  val _line3 = _line2.groupBy((x:(String, Int)) => x._1) // better to explicitly define dtype (dtype here is optional)
   println(_line3)
 
   // step 4) reduce
-  val _line4 = _line3.map(x => (x._1, x._2.length))
+  val _line4 = _line3.map((x:(String,List[(String,Int)])) => (x._1, x._2.size))  // better to explicitly define dtype (dtype here is optional)
   println(_line4)
 
   // step 5) sorting
-  val _line5 = _line4.toSeq.sortBy(x => -x._2)
+  //val _line5 = _line4.toList.sortBy(x => -x._2)
+  val _line5 = _line4.toSeq.sortBy((x:(String, Int)) => x._2).reverse
   println(_line5)
+
+  //---------------------
+  // do above in 1 command
+  //---------------------
+
+  val result = lines
+    .flatMap(s => s.split(" "))
+    .map(x => (x,1))
+    .groupBy((x:(String, Int)) => x._1)
+    .map((x:(String,List[(String,Int)])) => (x._1, x._2.size))
+    .toSeq.sortBy((x:(String, Int)) => x._2).reverse
+
+  println("result = " + result)
 }
